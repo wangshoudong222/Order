a7fcbc81059be9e937bc2edd3833004de184aee1
wangshoudong001 <wangshoudong001@ke.com>
2023-02-22 01:50:43 +0800

    增加主屏页面

---
+++ app/src/main/java/com/yun/orderPad/ui/SingleActivity.kt
@@ -0,0 +1,15 @@
+package com.yun.orderPad.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.yun.orderPad.R
+
+/**
+ * 零点模式
+ */
+class SingleActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_single)
+    }
+}
--- app/src/main/java/com/yun/orderPad/ui/WelcomeActivity.java
+++ app/src/main/java/com/yun/orderPad/ui/WelcomeActivity.java
@@ -8,6 +8,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class WelcomeActivity extends AppCompatActivity {
 
+  /**
+   * 主屏8”IPS,800x1280，电
+   * 容式多点触控
+   * 副屏4.95”IPS,480x960
+   * @param savedInstanceState
+   */
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
